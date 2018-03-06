f001fe8b5c8b4248f9e09c9fe674c9aa95d3e061
Tiago de Lacerda Gomes <tiagolacerdagomes@gmail.com>
2018-03-06 22:44:28 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # ProgramacaoVisual
 Repositório da Disciplina
+
+...
+
+git clone https://github.com/tiagolgomes/ProgramacaoVisual
+
+cd ProgramacaoVisual/
+
+mkdir source
+cd source
+
+mkdir ProgramacaoVisual
+cd ProgramacaoVisual
+
+dotnet new sln
+
+mkdir WebMvc
+cd WebMvc
+
+dotnet new mvc
+
+cd ..
+
+dotnet sln ProgramacaoVisual.sln add WebMvc/WebMvc.csproj
+...
+
+...
+
+echo
+git config --global user.email "tiagolacerdagomes@gmail.com"
+git config --global user.name "Tiago de Lacerda Gomes"
+
+git add *
+
+gitcommit -m "Initial commit"
+git push origin master
+...
---
+++ source/ProgramacaoVisual/WebMvc/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMvc.Models;
+
+namespace WebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static IList<Pessoa> PessoaList = new List<Pessoa>
+        {
+            new Pessoa {Id = 1, Nome = "Joao"},
+            new Pessoa {Id = 1, Nome = "Maria"},
+            new Pessoa {Id = 1, Nome = "John"}
+        };
+
+        public IList<Pessoa> GetPessoas()
+        {
+            return PessoaList;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
